dc4731a024bfe85bb0da168d896b4bcab12fd490
keto-12 <ketolomsadze@gmail.com>
2024-03-10 12:54:16 +0400

    Lecture 4 tasks moved to the new repository

---
+++ Lecture4/Task1.py
@@ -0,0 +1,18 @@
+#Lecture4_Task1
+# დაწერეთ პროგრამა რომელიც მიიღებს
+# მოთამაშეების რაოდენობას.
+# Პროგრამამ თითოეული მოთამაშისთვის უნდა დააგენერიროს
+# შემთხვევითი კამათლების წყვილი და დაბეჭდოს ეკრანზე. Მაგალითად:
+#Enter players number: 2
+#3 4
+#2 1
+
+import random
+
+number_of_players=int(input("Write a number of players:"))
+
+if number_of_players == 0:
+    print("Please, enter a number more than 0")
+
+for i in range(number_of_players):
+    print(random.randint(1,6),random.randint(1,6))
---
+++ Lecture4/Task2.py
@@ -0,0 +1,12 @@
+#Lecture4_Task2
+#დაწერეთ პროგრამა, რომელიც მიიღებს მთელ დადებით რიცხვს - n.
+# Პროგრამამ, უნდა დააგენერიროს n ცალი შემთვევით მთელი რიცხვი 0 – 1000 და
+# ეკრანზე დაბეჭდოს მათ შორის მაქსიმალური. 0 < n < 30.
+
+import random
+
+number_input = int(input("Please, enter a positive integer between 0 and 30:"))
+if number_input==0 or number_input>30:
+    print("Please, enter a positive integer between 0 and 30")
+else:
+    print(max([random.randint(0,1000)for i in range(number_input) ]))
