ad2ef7619e390e2c6e5c875c09c57405a648dd28
keto-12 <ketolomsadze@gmail.com>
2024-03-11 15:15:56 +0400

    Lecture 4 task 4 solved

--- Lecture4/Task4.py
+++ Lecture4/Task4.py
@@ -5,3 +5,25 @@
 # ხოლო ყოველი მომდევნო წევრი არის წინა ორი წევრის ჯამი. Მაგალითად:
 #Enter number: 4
 #3
+def fib (n):
+
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        a = 0
+        b = 1
+        for i in range(1,n):
+             c = a + b
+             a = b
+             b = c
+        return (b)
+
+sequence_number = int(input("Please enter an integer between 0 and 20:"))
+if sequence_number<0 or sequence_number>20:
+    print("Please, enter an integer between 0 and 20:")
+else:
+    print(fib(sequence_number))
+
+
