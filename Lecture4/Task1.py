09a72c758d8ae9df87180086f1f384524b3b2f1f
keto-12 <ketolomsadze@gmail.com>
2024-03-11 09:59:20 +0400

    Lecture 4 updated

--- Lecture4/Task1.py
+++ Lecture4/Task1.py
@@ -13,6 +13,8 @@ number_of_players=int(input("Write a number of players:"))
 
 if number_of_players == 0:
     print("Please, enter a number more than 0")
+if number_of_players <0:
+    print("Please enter a positive number")
 
 for i in range(number_of_players):
     print(random.randint(1,6),random.randint(1,6))
--- Lecture4/Task2.py
+++ Lecture4/Task2.py
@@ -8,5 +8,7 @@ import random
 number_input = int(input("Please, enter a positive integer between 0 and 30:"))
 if number_input==0 or number_input>30:
     print("Please, enter a positive integer between 0 and 30")
+elif number_input<0:
+    print("Please, enter a positive integer between 0 and 30")
 else:
     print(max([random.randint(0,1000)for i in range(number_input) ]))
