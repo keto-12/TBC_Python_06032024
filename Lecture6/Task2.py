4b289f945a152c6002406a3d67f66676e403d793
keto-12 <ketolomsadze@gmail.com>
2024-03-14 19:45:16 +0400

    Lecture 6 tasks

--- Lecture6/Task2.py
+++ Lecture6/Task2.py
@@ -5,7 +5,7 @@
 DB_PASSWORD ="Georgia"
 MAX_TRIES=3
 
-for i in range (1,4):
+for i in range (1,MAX_TRIES+1):
     input_password = input("Please enter password:")
     if (input_password == DB_PASSWORD and i<=3):
         print("Hello from Georgia")
